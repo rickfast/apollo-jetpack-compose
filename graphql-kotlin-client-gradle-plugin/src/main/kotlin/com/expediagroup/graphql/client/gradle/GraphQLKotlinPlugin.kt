6a773763ed15c1d96db6d0a73c07909f5e3c5c4d
rfast <you@example.com>
2019-12-30 17:51:19 -0600

    start gradle plugin

---
+++ graphql-kotlin-client-gradle-plugin/src/main/kotlin/com/expediagroup/graphql/client/gradle/GraphQLKotlinPlugin.kt
@@ -0,0 +1,10 @@
+package com.expediagroup.graphql.client.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GraphQLKotlinPlugin : Plugin<Project> {
+    override fun apply(p0: Project?) {
+        
+    }
+}
